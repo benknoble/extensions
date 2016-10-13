dd62dfbed0a637ca521f094602291d96b0b9c34f
benknoble <ben.knoble@gmail.com>
2016-10-12 21:43:44 -0400

    Added extension method ToEnum (this string s)

--- Extensions/MyClass.cs
+++ Extensions/MyClass.cs
@@ -1,9 +1,32 @@
 ﻿// MyClass.cs in David Knoble's Project 
 
+using System;
+
 namespace Knoble.Extensions
 {
 	public static class Extensions
 	{
 		//TODO: create extensions
+
+		/// <summary>
+		/// Converts a string to an enumerated type.
+		/// </summary>
+		/// <returns>The enum value.</returns>
+		/// <param name="s">S.</param>
+		/// <typeparam name="TEnum">The enumerated type.</typeparam>
+		/// <exception cref="ArgumentException">Thrown when TEnum is not an enumerated type.</exception>
+		public static TEnum ToEnum<TEnum> (this string s) where TEnum : struct
+		{
+			if (!typeof (TEnum).IsEnum)
+				throw new ArgumentException ($"{nameof (TEnum)} must be an enumerated type");
+
+			TEnum t;
+			if (Enum.TryParse (s, out t))
+			{
+				return t;
+			}
+
+			return default (TEnum);
+		}
 	}
 }
