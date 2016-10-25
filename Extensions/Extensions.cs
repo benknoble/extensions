2ca26332e6c816f61f03f0bc192d629c1f4c20ec
benknoble <ben.knoble@gmail.com>
2016-10-24 20:22:32 -0400

    * Extensions/Extensions.cs: Add extension methods to DateTime struct   to get random dates (see RandomDate methods in Knoble.Utils)
    * Extensions/Extensions.csproj: Add reference to Extensions.Utils
      project

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -28,5 +28,28 @@ namespace Knoble.Extensions
 
 			return default (TEnum);
 		}
+
+		/// <summary>
+		/// Returns a random date between this date and the specified date.
+		/// </summary>
+		/// <returns>The date.</returns>
+		/// <param name="from">From.</param>
+		/// <param name="to">To.</param>
+		public static DateTime RandomDateUpTo (this DateTime from, DateTime to)
+		{
+			return Utils.Utils.RandomDateInRange (from, to);
+		}
+
+		/// <summary>
+		/// Returns a random date between this date and now. By default, uses DateTime.Now.
+		/// Uses DateTime.UtcNow if Utc passed as true.
+		/// </summary>
+		/// <returns>The date.</returns>
+		/// <param name="from">From.</param>
+		/// <param name="Utc">If set to <c>true</c> UTC.</param>
+		public static DateTime RandomDate (this DateTime from, bool Utc = false)
+		{
+			return Utils.Utils.RandomDate (from, Utc);
+		}
 	}
 }
