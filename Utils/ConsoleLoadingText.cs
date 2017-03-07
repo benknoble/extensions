9be454d5026decd2f1f6991c438b6883cdb7ddf8
benknoble <ben.knoble@gmail.com>
2017-03-07 14:57:06 -0500

    Fixes #1
    Incorporate CodeReview/SO changes (see #1)

--- Utils/ConsoleLoadingText.cs
+++ Utils/ConsoleLoadingText.cs
@@ -5,94 +5,67 @@ using System.Threading.Tasks;
 
 namespace Knoble.Utils
 {
-	/// <summary>
-	/// A class that represents a possibily infinitely looping load screen.
-	/// It displays a product name, loading text, and spinner that spins for a given delay.
-	/// </summary>
-	public class ConsoleLoadingText
-	{
+    /// <summary>
+    /// A class that represents a possibily infinitely looping load screen.
+    /// It displays a product name, loading text, and spinner that spins for a given delay.
+    /// </summary>
+    public class ConsoleLoadingText
+    {
+        /// <summary>
+        /// A default product name.
+        /// </summary>
 		public const string DefaultProductName = "";
+        /// <summary>
+        /// A default loading text that can be used.
+        /// </summary>
 		public const string DefaultLoadingText = "Loading...";
+        /// <summary>
+        /// A default milliseconds delay of 250 ms.
+        /// </summary>
 		public const int DefaultMillisecondsDelay = 250;
 
-		static string[] spinner = { "|", "/", "-", @"\" };
+        static readonly string[] spinner = { "|", "/", "-", @"\" };
 
-		readonly string productName, loadingText;
-		readonly int millisecondsDelay;
+        readonly string productName, loadingText;
+        readonly int millisecondsDelay;
 
-		int i;
-		bool @continue = true;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Knoble.Utils.Loading"/> class.
+        /// Displays "{productName} {loadingText} x" where the spinner (x) spins every {millisecondsDelay} milliseconds.
+        /// </summary>
+        /// <param name="productName">Product name.</param>
+        /// <param name="loadingText">Loading text.</param>
+        /// <param name="millisecondsDelay">Milliseconds delay.</param>
+        public ConsoleLoadingText (string productName, string loadingText, int millisecondsDelay)
+        {
+            if (productName == null)
+                throw new ArgumentException (nameof (productName));
+            if (loadingText == null)
+                throw new ArgumentException (nameof (loadingText));
+            if (millisecondsDelay < 0)
+                throw new ArgumentException (nameof (millisecondsDelay));
+            this.productName = productName;
+            this.loadingText = loadingText;
+            this.millisecondsDelay = millisecondsDelay;
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="T:Knoble.Utils.Loading"/> class.
-		/// Defaults to displaying "Loading... x" where the spinner (x) spins every quarter second.
-		/// </summary>
-		public ConsoleLoadingText () : this (DefaultProductName)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="T:Knoble.Utils.Loading"/> class.
-		/// Defaults to displaying "{productName} Loading... x" where the spinner (x) spins every quarter second.
-		/// </summary>
-		/// <param name="productName">Product name.</param>
-		public ConsoleLoadingText (string productName) : this (productName, DefaultLoadingText)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="T:Knoble.Utils.Loading"/> class.
-		/// Defaults to displaying "{productName} {loadingText} x" where the spinner (x) spins every quarter second.
-		/// </summary>
-		/// <param name="productName">Product name.</param>
-		/// <param name="loadingText">Loading text.</param>
-		public ConsoleLoadingText (string productName, string loadingText) : this (productName, loadingText, DefaultMillisecondsDelay)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="T:Knoble.Utils.Loading"/> class.
-		/// Displays "{productName} {loadingText} x" where the spinner (x) spins every {millisecondsDelay} milliseconds.
-		/// </summary>
-		/// <param name="productName">Product name.</param>
-		/// <param name="loadingText">Loading text.</param>
-		/// <param name="millisecondsDelay">Milliseconds delay.</param>
-		public ConsoleLoadingText (string productName, string loadingText, int millisecondsDelay)
-		{
-			if (productName == null)
-				throw new ArgumentException (nameof (productName));
-			if (loadingText == null)
-				throw new ArgumentException (nameof (loadingText));
-			if (millisecondsDelay < 0)
-				throw new ArgumentException (nameof (millisecondsDelay));
-			this.productName = productName;
-			this.loadingText = loadingText;
-			this.millisecondsDelay = millisecondsDelay;
-		}
-
-		/// <summary>
-		/// Returns a task that, when running, continously prints the loading text.
-		/// </summary>
-		public Task Display ()
-		{
-			return Task.Run (() =>
-			{
-				@continue = true;
-				while (@continue)
-				{
-					Console.Write ($"\r{productName} {loadingText} {spinner[i]}");
-					i = (i + 1) % spinner.Length;
-					Thread.Sleep (millisecondsDelay);
-				}
-			});
-		}
-
-		/// <summary>
-		/// Stop this instance from displaying.
-		/// </summary>
-		public void Stop ()
-		{
-			@continue = false;
-		}
-	}
+        /// <summary>
+        /// Returns a task that, when running, continously prints the loading text.
+        /// </summary>
+        public Task Display (CancellationToken ct)
+        {
+            return Task.Run (
+                () =>
+                {
+                    int i = 0;
+                    while (!ct.IsCancellationRequested)
+                    {
+                        Console.Write ($"\r{productName} {loadingText} {spinner[i]}");
+                        i = (i + 1) % spinner.Length;
+                        Thread.Sleep (millisecondsDelay);
+                    }
+                },
+            cancellationToken: ct);
+        }
+    }
 }
